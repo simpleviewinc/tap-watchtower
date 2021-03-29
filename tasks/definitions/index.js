0277f1c78d79cb273824ff059e937bb6cfe4edf6
mpcarolin <mpcarolin.dev@gmail.com>
2021-03-29 10:52:35 -0700

    removed unneeded code

--- tasks/definitions/index.js
+++ tasks/definitions/index.js
@@ -1,25 +1,3 @@
-const { isObj, isFunc, mapObj } = require('@keg-hub/jsutils')
-
-const injectWatchtowerConfig = taskAction => {
-  return args => taskAction({
-    ...args,
-    watchtowerConfig: {}
-  })
-}
-
-const initialize = tasks => {
-  mapObj(tasks, (_, task) => {
-    task.action = isFunc(task.action) 
-      ? injectWatchtowerConfig(task.action)
-      : undefined,
-    task.tasks = isObj(task.tasks)
-      ? initialize(task.tasks)
-      : undefined
-  })
-
-  return tasks
-}
-
 module.exports = {
-  ...initialize(require('./container')),
+  ...require('./container')
 }
