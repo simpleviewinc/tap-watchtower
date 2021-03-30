d7c6213e361c01aa932600e982032b5eb8bb92f0
mpcarolin <mpcarolin.dev@gmail.com>
2021-03-30 13:21:03 -0700

    added .kegConfig path to getConfig search

--- tasks/utils/getConfig.js
+++ tasks/utils/getConfig.js
@@ -5,18 +5,24 @@ const path = require('path')
 const appRoot = require('app-root-path').path
 
 const HOME_DIR = os.homedir()
+const KEG_CONFIG_DIR = path.join(HOME_DIR, '.kegConfig')
+const FILE_NAME = 'watchtower.config'
 
-// paths to tap-watchtower configuration files
+// paths to tap-watchtower configuration files where
+// entries at the end take precedent over entries at the start
 const configPaths = [
-  path.join(appRoot,'configs/watchtower.config.json'),
-  path.join(appRoot,'configs/watchtower.config.js'),
-  path.join(HOME_DIR, 'watchtower.config.json'),
-  path.join(HOME_DIR,'watchtower.config.js'),
+  path.join(appRoot, 'configs', `${FILE_NAME}.json`),
+  path.join(appRoot, 'configs', `${FILE_NAME}.js`),
+  path.join(KEG_CONFIG_DIR, `${FILE_NAME}.json`),
+  path.join(KEG_CONFIG_DIR, `${FILE_NAME}.js`),
+  path.join(HOME_DIR, `${FILE_NAME}.json`),
+  path.join(HOME_DIR,`${FILE_NAME}.js`),
 ]
 
 module.exports = {
   /**
-   * @param  {...string} paths - paths to configuration files (later params take precedence)
+   * @param  {...string} paths - optional paths to configuration files 
+   *  - these take precedent over the default locations -- see `configPaths` array
    * @returns {object} the merged configuration object from all the paths
    */
   getConfig: (...paths) => deepMerge(
